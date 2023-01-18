e45e295d586c5c399f1908d639e89fa6c33e02a0
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-01-18 13:11:53 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,220 @@
+<script>
+  
+import { onMount } from "svelte";
+import Tooltip from "./Tooltip.svelte";
+import { slide } from 'svelte/transition';
+
+$: data = {
+  points: [],
+  summarised: {
+    max: 0,
+    min: 0,
+    export: 0,
+    import: 0,
+    netto: 0,
+    exportPercent: 0,
+    importPercent: 0
+  },
+  timespan: {
+    from: "0",
+    to: "0"
+  },
+  css: {
+    zero: 0
+  }
+}
+
+// Get data
+onMount(async () => {
+  fetch("https://statnett-utveksling.vercel.app/api")
+    .then(r => r.json())
+    .then(d => {
+      console.log(d.data)
+      build(d.data)
+    })
+    .catch(e => { console.log(e) })
+})
+
+function build(d) {
+  let span = d.SidebarViewModel.max + Math.abs(d.SidebarViewModel.min)
+  let zero = d.SidebarViewModel.max / span * 100
+  function absToBar(value, index, start) {
+    let date = new Date(start + (index * 86400000))
+    let bar = {
+      a: value,
+      h: Math.abs(value) / span * 100,
+      c: value > 0 ? "pos" : "neg",
+      d: date.toLocaleDateString(undefined, {month: 'short', day: 'numeric'})
+    }
+    return bar
+  }
+  let bars = d.PhysicalFlowNetExchange.map((p, i) => absToBar(p, i, d.StartPointUTC))
+  data = {
+    points: bars,
+    summarised: d.SidebarViewModel,
+    timespan: {
+      from: d.StartPointUTC,
+      to: d.EndPointUTC
+    },
+    css: {
+      zero: zero
+    }
+  }
+  console.log(data)
+}
+
+/* function localDate(utc) {
+  let d = new Date(utc)
+  return d.toLocaleDateString()
+} */
+
+</script>
+
+<div class="exchange-app">
+  <h2>Live: Kraftutveksling med utlandet hittil i år</h2>
+  <div class=exchange>
+    <div>
+      <h3>Eksport</h3>
+      <div class="twh">{(data.summarised.export / 1000000).toLocaleString(undefined, { maximumFractionDigits: 2 })} TWh</div>
+    </div>
+    <div class=percentage>
+      <div>
+        <div class=export style="width: {data.summarised.exportPercent}%;">{(data.summarised.exportPercent).toFixed(0)}%</div>
+        <div class=import style="width: {data.summarised.importPercent}%;">{(data.summarised.importPercent).toFixed(0)}%</div>
+      </div>
+    </div>
+    <div>
+      <h3>Import</h3>
+      <div class="twh">{(data.summarised.import / 1000000).toLocaleString(undefined, { maximumFractionDigits: 2 })} TWh</div>
+    </div>
+  </div>
+  <div class="exchange-per-day" style="--zero: {data.css.zero}%">
+    <h3>Balanse per dag</h3>
+    <p>Mørke stolper betyr at det er eksportert mer enn det er importert denne dagen.</p>
+    <div class="days">
+      {#each data.points as point, i}
+      <div class="day">
+        <Tooltip title="{point.d}: {(point.a).toLocaleString(undefined, { maximumFractionDigits: 0 })} MWh">
+          <div class="bar {point.c}" style="height: {point.h}%;" transition:slide="{{ delay: 50 * i, duration: 1000 }}"></div>
+        </Tooltip>
+      </div>
+      {/each}
+    </div>
+  </div>
+  <div class="meta">
+    Oppdatert {(new Date(data.timespan.to)).toLocaleDateString(undefined, {year: 'numeric', month: 'long', day: 'numeric'})}. Kilde: Statnett.
+  </div>
+</div>
+
+<style>
+.exchange-app {
+  --import: #d9d4b6;
+  --export: #404040;
+}
+.exchange {
+  display: flex;
+  gap: 20px;
+}
+.percentage {
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+.percentage > div {
+  display: flex;
+  width: 100%;
+}
+.import, .export {
+  padding: 10px 15px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  transition: .5s ease-out;
+}
+.export {
+  background: var(--export);
+  color: white;
+  border-top-left-radius: 99px;
+  border-bottom-left-radius: 99px;
+}
+.import {
+  justify-content: flex-end;
+  background: var(--import);
+  border-top-right-radius: 99px;
+  border-bottom-right-radius: 99px;
+}
+h3, p {
+  margin: 0 0 5px;
+}
+.twh {
+  font-weight: bold;
+}
+.exchange-per-day {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.days {
+  display: flex;
+  gap: 1%;
+  height: 150px;
+  margin-top: 15px;
+  padding-right: 55px;
+  position: relative;
+}
+.day {
+  flex: 1;
+  height: 100%;
+  max-width: 20px;
+  position: relative;
+}
+.day:first-child::before {
+  position: absolute;
+  bottom: -20px;
+  width: 100px;
+  font-size: .8em;
+  color: #404040;
+  content: "1. jan.";
+}
+.day:last-of-type::before {
+  position: absolute;
+  bottom: -20px;
+  right: 0;
+  text-align: right;
+  width: 100px;
+  font-size: .8em;
+  color: #404040;
+  content: "18. jan.";
+}
+.bar {
+  position: absolute;
+  top: var(--zero);
+  width: 100%;
+}
+.bar.pos {
+  background: var(--export);
+  transform: translateY(-100%);
+  border-top-left-radius: 99px;
+  border-top-right-radius: 99px;
+}
+.bar.neg {
+  background: var(--import);
+  border-bottom-left-radius: 99px;
+  border-bottom-right-radius: 99px;
+}
+.bar:hover {
+  filter:opacity(0.7)
+}
+.meta {
+  margin-top: 40px;
+  font-size: 0.9em;
+}
+/* .import::before {
+  position: absolute;
+  top: -3px;
+  left: 0;
+  width: 2px;
+  background: white;
+  height: calc(100% + 6px);
+  content: "";
+} */
+</style>
---
+++ src/Tooltip.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let title = ''
+	let isHovered = false,
+        x, 
+        y
+	
+	function mouseOver(event) {
+		isHovered = true
+		x = event.pageX
+		y = event.pageY
+	}
+	function mouseMove(event) {
+		x = event.pageX
+		y = event.pageY
+	}
+	function mouseLeave() {
+		isHovered = false
+	}
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div
+	on:mouseover={mouseOver}
+    on:mouseleave={mouseLeave}
+	on:mousemove={mouseMove}>
+	<slot />
+</div>
+
+{#if isHovered}
+	<div style="top: {y + 20}px; left: {x}px;" class="tooltip">{title}</div>
+{/if}
+
+<style>
+	.tooltip {
+		background: #404040;
+        font-size: .9em;
+        color: white;
+		border-radius: 99px;
+		padding: 6px 8px;
+		position: fixed;
+        z-index: 1000;
+        transform: translateX(-50%);
+	}
+</style>
